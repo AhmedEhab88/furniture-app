1aa608b435bd826ecb06406ec9d753ae8f31e269
AhmedEhab88 <ahmedesrm@gmail.com>
2023-08-05 02:13:20 +0300

    Added search screen

--- screens/Search.jsx
+++ screens/Search.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Ionicons, Feather } from '@expo/vector-icons';
+import { TextInput } from 'react-native-gesture-handler';
+import { COLORS, SIZES } from '../constants/index';
+import styles from './search.style';
 
 function Search() {
     return (
         <SafeAreaView>
-            <Text>Search</Text>
+            <View style={styles.searchContainer}>
+                <TouchableOpacity>
+                    <Ionicons
+                        name="camera-outline"
+                        size={SIZES.xLarge}
+                        style={styles.searchIcon}
+                    />
+                </TouchableOpacity>
+                <View style={styles.searchWrapper}>
+                    <TextInput
+                        style={styles.searchInput}
+                        onPressIn={() => {}}
+                        placeholder="What are you looking for?"
+                    />
+                </View>
+                <View>
+                    <TouchableOpacity style={styles.searchBtn}>
+                        <Feather
+                            name="search"
+                            size={24}
+                            color={COLORS.offwhite}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </View>
         </SafeAreaView>
     );
 }
 export default Search;
-const styles = StyleSheet.create({});
